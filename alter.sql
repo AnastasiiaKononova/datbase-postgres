a85c82759732f718aef2291a589f15b5e074e6d6
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-01-29 15:25:27 +0200

    Alter

---
+++ alter.sql
@@ -0,0 +1,20 @@
+CREATE TABLE dogs (
+    name VARCHAR(300),
+    breed VARCHAR(300),
+    color VARCHAR(300)
+);
+
+INSERT INTO dogs (name, breed, color) VALUES 
+('Bark', 'None', 'Red'), 
+('Doggi', 'Alabai', 'Black'), 
+('Bark', 'None', 'Red');
+
+ALTER TABLE dogs 
+-- далі іде дія
+ADD COLUMN id serial;
+
+ALTER TABLE dogs
+DROP COLUMN id;
+
+ALTER TABLE dogs
+ADD CONSTRAINT "dogs_pkey" PRIMARY KEY (id);
